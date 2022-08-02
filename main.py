7985ae4d15dfe9c86a3a75204f3606bb9c225bce
indira923 <108821566+indira923@users.noreply.github.com>
2022-08-02 16:18:50 -0400

    Add Indira changes after revert

--- main.py
+++ main.py
@@ -4,6 +4,33 @@
 #indira
 #nour
 
+import random
+#Benny
+#testing
+#indira
+
+num1 = random.randint(1,100)
+num2 = random.randint(1,100)
+num3 = random.randint(-100,50)
+num4 = random.randint(-100,100)
+num5 = random.randint(1,100)
+num6 = random.randint(1,100)
+eigth_questions = {
+    "question1": f"What is the greatest common factor of {num1} and {num2}?",
+    "question2" :f"{num1}+x={num2}",
+    "question3" : f"{num2}+x={num1}"
+}
+seventh_questions = {
+    "question1" : "{num3} - {num4}" 
+    "question2" : "{num4} + {num3}" 
+    "question3" : "{num3}+ {num4}"
+}
+sixith_questions  = {
+    "question1" : "{num5}**2"
+    "question2" : "{num6}**3"
+    "question3" : "{num6}**4"
+}
+
 def generate_question():
     pass
     
