c32a2ee217e26f09084d2754d668f24ac352b918
sonicjc90 <juan.medina@csedge.org>
2022-08-02 16:02:04 -0400

    spooderman

--- main.py
+++ main.py
@@ -3,3 +3,21 @@
 #testing
 #indira
 #nour
+
+def generate_ans(ans):
+    
+def add():
+    ans = num1 + num2
+    return 
+
+def subtract():
+    ans = num1 - num2
+    return 
+
+def divide():
+    ans = num1 // num2
+    return 
+
+def multiply():
+    ans = num1*num2
+    return 
