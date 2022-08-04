1f2fc9c149fa1ed47fd465f664d01eaa231eb00b
sonicjc90 <juan.medina@csedge.org>
2022-08-04 15:58:21 -0400

    still ain't finished kill me now

--- testing.py
+++ testing.py
@@ -15,3 +15,11 @@ num3 = num1*variable_z+num2
 
 print(f"{num3} = {num1}z + {num2}. Solve for z.")
 print(variable_z)
+
+eigth_questions = {
+    "question1": f"What is the greatest common factor of {num1} and {num2}?",
+    "question2" :f"{num1}{x} + {num2} = {sum1}. What is 'x'?",
+    "question3" : f"{num2}{x} - {num1} = {dif1}. What does 'x' equal to?"
+}
+
+print(eigth_questions["question1"])
